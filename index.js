ab00a3ea334d111c923bac8de5169f5d3ecc1344
Bezouro <bruno@bezouro.com.br>
2020-07-13 17:57:50 -0300

    Added the possibility to use an existing webserver

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const DEFAULT_VERSION = '1.15.2'
 
-module.exports = function (bot, port = 3000) {
-  const path = require('path')
+module.exports = function (bot, options) {
+  options = options || {}
+  let path = options.path || require('path')
+  let express = options.express || require('express')
+  let app = options.app || express()
+  let http = options.http || require('http').createServer(app)
+  let io = options.io || require('socket.io').listen(http)
+  let port = options.port || 3000
+
   const _ = require('lodash')
-  const express = require('express')
-
-  const app = express()
-  const http = require('http').createServer(app)
-
-  const io = require('socket.io')(http)
 
   // Try to load mcAssets
   let mcAssets = require('minecraft-assets')(bot.version)
@@ -24,7 +25,7 @@ module.exports = function (bot, port = 3000) {
 
   app.use('/public', express.static(path.join(__dirname, 'public')))
 
-  app.get('/', (req, res) => {
+  app.get(path, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
   })
 
