5d12b54c14c1734c1ac6efb4a55fcf2597ef9988
imharvol <imharvol@gmail.com>
2021-08-02 09:36:48 +0200

    Support multiple windows on the frontend
    The HTML canvas can now draw inventories, chests, double chests, furnaces and crafting tables.
    
    I'll take a look again to check for bugs and improvements, like this one:
    ```
    const newSlots = { ...windowComponent.window.slots, ...windowUpdate.slots }
    windowComponent.window = { ...windowComponent.window, slots: newSlots }
    ```
    public/index.js:14

--- public/index.js
+++ public/index.js
@@ -1,100 +1,64 @@
-/* global io, Vue, Image, _ */
+/* global io, Vue, Image, _, windowSlotsCoords */
 
 const socket = io()
 
-socket.on('inventory', function (inv) {
-  inventoryComponent.inventory = inv
+socket.on('window', function (window) {
+  windowComponent.window = window
 })
 
-socket.on('inventoryUpdate', function (inventoryUpdate) {
-  for (const slot in inventoryUpdate) {
-    const newItem = inventoryUpdate[slot]
+socket.on('windowUpdate', function (windowUpdate) {
+  if (windowUpdate.id !== windowComponent.window.id) return
 
-    const itemIndex = inventoryComponent.inventory.findIndex(e => e.slot === parseInt(slot))
-
-    if (itemIndex >= 0) {
-      if (!newItem) { // If the item has been removed
-        inventoryComponent.inventory.splice(itemIndex, 1)
-      } else {
-        Vue.set(inventoryComponent.inventory, itemIndex, newItem)
-      }
-    } else {
-      if (newItem) {
-        inventoryComponent.inventory.push(newItem)
-      }
-    }
-  }
+  // TODO: This works but it's not the best. For some reason the watch function was not
+  // being called when a slot changed, I tried to use Vue.set() but I couldn't get it to work
+  const newSlots = { ...windowComponent.window.slots, ...windowUpdate.slots }
+  windowComponent.window = { ...windowComponent.window, slots: newSlots }
 })
 
-const inventoryComponent = new Vue({
-  el: '#inventory',
+const windowComponent = new Vue({
+  el: '#window',
   data: {
-    inventory: [],
+    window: { id: null, type: null, slots: {} },
     showJSON: false
   },
+  computed: {
+    isEmpty: function () {
+      return !!Object.values(this.window.slots).findIndex(e => e ?? false)
+    }
+  },
   watch: {
-    inventory: function (newInventory) {
-      this.throttleDrawInventory(newInventory)
+    window: function (newWindow) {
+      console.log(newWindow)
+      this.throttleDrawWindow(newWindow)
     }
   },
   created: function () {
-    this.throttleDrawInventory = _.throttle(drawInventory, 100)
+    this.throttleDrawWindow = _.throttle(drawWindow, 100)
   }
 })
 
-// Might be better to hardcode all the values
-const inventorySlots = {
-  // Crafting slots
-  0: [307, 55],
-  1: [195, 35],
-  2: [231, 35],
-  3: [195, 71],
-  4: [231, 71],
-
-  5: [15, 15], // Helmet
-  9: [15, 167], // Inventory
-  18: [15, 203], // Inventory
-  27: [15, 239], // Inventory
-  36: [15, 283], // Tool bar
-  45: [153, 123] // Left hand
-}
-
-// Add tool bar slots (36 - 44)
-for (let i = 36 + 1; i <= 44; i++) {
-  inventorySlots[i] = [inventorySlots[i - 1][0] + 36, inventorySlots[i - 1][1]]
-}
-
-// Add armor slots (5 - 8)
-for (let i = 5 + 1; i <= 8; i++) {
-  inventorySlots[i] = [inventorySlots[i - 1][0], inventorySlots[i - 1][1] + 36]
-}
-
-// Add inventory slots (9 - 35)
-for (let i = 9 + 1; i <= 17; i++) {
-  inventorySlots[i] = [inventorySlots[i - 1][0] + 36, inventorySlots[i - 1][1]]
-}
-for (let i = 18 + 1; i <= 26; i++) {
-  inventorySlots[i] = [inventorySlots[i - 1][0] + 36, inventorySlots[i - 1][1]]
-}
-for (let i = 27 + 1; i <= 35; i++) {
-  inventorySlots[i] = [inventorySlots[i - 1][0] + 36, inventorySlots[i - 1][1]]
-}
-
 // CANVAS
-function drawInventory (inventory) {
-  const canvas = document.getElementById('inventoryCanvas')
+function drawWindow (window) {
+  const canvas = document.getElementById('windowCanvas')
   const ctx = canvas.getContext('2d')
 
   // Draw background
-  ctx.drawImage(document.getElementById('inventoryImage'), 0, 0)
+  const windowImage = new Image()
+  windowImage.addEventListener('load', function () {
+    canvas.width = windowImage.width
+    canvas.height = windowImage.height
+    ctx.drawImage(windowImage, 0, 0)
+  })
+  windowImage.src = `/public/windows/${window?.type ?? 'inventory'}.png`
 
   // Draw items
-  for (const item in inventory) {
-    const inventorySlot = inventorySlots[inventory[item].slot]
+  for (const item in window.slots) {
+    if (!window.slots[item]) continue
+    const inventorySlot = windowSlotsCoords[window.type][window.slots[item].slot]
 
-    if (inventory[item].texture && inventorySlot) {
+    if (window.slots[item].texture && inventorySlot) {
       const itemImage = new Image()
-      itemImage.src = inventory[item].texture
+      itemImage.src = window.slots[item].texture
 
       itemImage.onload = function () {
         // Draw item image
@@ -102,13 +66,13 @@ function drawInventory (inventory) {
         ctx.drawImage(itemImage, inventorySlot[0], inventorySlot[1], 32, 32)
 
         // Draw item count
-        if (inventory[item].count > 1) {
+        if (window.slots[item].count > 1) {
           ctx.font = '20px monospace'
           ctx.fillStyle = 'black'
           ctx.textAlign = 'end'
-          ctx.fillText(inventory[item].count, inventorySlot[0] + 33, inventorySlot[1] + 31)
+          ctx.fillText(window.slots[item].count, inventorySlot[0] + 33, inventorySlot[1] + 31)
           ctx.fillStyle = 'white'
-          ctx.fillText(inventory[item].count, inventorySlot[0] + 32, inventorySlot[1] + 30)
+          ctx.fillText(window.slots[item].count, inventorySlot[0] + 32, inventorySlot[1] + 30)
         }
       }
     }
