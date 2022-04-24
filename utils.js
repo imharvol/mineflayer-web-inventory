3b8405456f882687bfe5379bb33a31c21dbbc74c
imharvol <ImHarvol@gmail.com>
2022-04-24 12:30:28 +0200

    Fix "metadata textures and names" on version 1.12.2 and lower (#29)
    * Get metadata textures programatically
    
    This seems to work with most items

--- utils.js
+++ utils.js
@@ -1,3 +1,8 @@
+const path = require('path')
+const fs = require('fs')
+
+const rawMcData = require('minecraft-data')
+
 const windowNamesOrig = {
   inventory: ['minecraft:inventory'],
   chest: ['minecraft:generic_9x3', 'minecraft:chest'],
@@ -28,7 +33,43 @@ function getWindowName (window) {
   }
 
   if (Object.keys(windowNames).includes(window.type)) return window.type
-  return Object.keys(windowNames)[Object.values(windowNames).findIndex(e => e.includes(window.type))]
+  return Object.keys(windowNames)[
+    Object.values(windowNames).findIndex((e) => e.includes(window.type))
+  ]
 }
 
-module.exports = { getWindowName, setFailStreak }
+function addTexture (mcData, mcAssets, item) {
+  if (!item) return item
+
+  const blockModels = JSON.parse(fs.readFileSync(path.join(mcAssets.directory, 'blocks_models.json')))
+
+  if (mcData.version['<=']('1.12.2')) {
+    // Fixes the name
+    const itemVariations = mcData.itemsByName[item.name]?.variations ?? mcData.blocksByName[item.name]?.variations
+    if (itemVariations) { item.displayName = itemVariations.find(variation => variation.metadata === item.metadata).displayName }
+
+    // Tries to fix the texture
+    let minecraftName =
+      rawMcData.legacy.pc.items[item.type + ':' + item.metadata].substr('minecraft:'.length)
+    if (minecraftName.includes('[')) minecraftName = minecraftName.substr(0, minecraftName.indexOf['['] - 1)
+
+    if (blockModels[minecraftName]) {
+      const assetName = Object.values(blockModels[minecraftName].textures)[0]
+
+      try {
+        const textureBase64 = fs
+          .readFileSync(path.join(mcAssets.directory, assetName + '.png'))
+          .toString('base64')
+        item.texture = 'data:image/png;base64,' + textureBase64
+      } catch (err) {
+        // It wasn't found. This happens with pistons for example
+      }
+    }
+  }
+
+  if (!item.texture) item.texture = mcAssets.textureContent[item.name].texture
+
+  return item
+}
+
+module.exports = { getWindowName, setFailStreak, addTexture }
